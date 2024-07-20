dd18b02b63931ec506998ef97d54d0157faf8f56
Jeon-jisu <wltnjeon0119@gmail.com>
2024-07-20 23:56:36 +0900

    [Feature] config.yaml 파일 반영하도록 변경, combined image 생성 #16 #19

--- ControlNet2/tutorial_train_custom.py
+++ ControlNet2/tutorial_train_custom.py
@@ -1,3 +1,10 @@
+"""
+
+다음과 같은 명령어로 실행시킬 수 있습니다. 
+python train.py --config config/config1.yaml
+
+"""
+
 from share import *
 import torch
 import pytorch_lightning as pl
@@ -10,51 +17,62 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 import torch.nn as nn
 import wandb
 from pytorch_lightning.loggers import WandbLogger
+from utils import load_config
+import argparse
+import os
 
-# Configs
-resume_path = './models/control_sd15_customv2.ckpt'
-batch_size = 4
-logger_freq = 300
-learning_rate = 1e-5
-sd_locked = True
-only_mid_control = False
+def main(config_path):
+    config = load_config(config_path)
+    # WandB 설정
+    wandb.init(project=config['wandb_project'], name=config['wandb_name'])
+    wandb_logger = WandbLogger(project=config['wandb_project'])
 
-# WandB 설정
-wandb.init(project="3difftection", name="init_train_v3_Normalize_Change_onetoone")
-wandb_logger = WandbLogger(project="3difftection")
- 
-# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
-model = create_model('./models/cldm_v15v2.yaml').cpu()
+    # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+    model = create_model(config['model_config_path']).cpu()
 
-model.load_state_dict(load_state_dict(resume_path, location='cpu'))
-model.learning_rate = learning_rate
-model.sd_locked = sd_locked
-model.only_mid_control = only_mid_control
+    # Config setting
+    model.load_state_dict(load_state_dict(config['resume_path'], location='cpu'))
+    model.learning_rate = config['learning_rate']
+    model.sd_locked = config['sd_locked']
+    model.only_mid_control = config['only_mid_control']
 
-# ModelCheckpoint callback
-checkpoint_callback = ModelCheckpoint(
-    monitor='train_loss',  # 모니터링할 메트릭
-    dirpath='./checkpoints',  # 체크포인트가 저장될 디렉토리
-    filename='model-{epoch:02d}-{train_loss:.2f}',  # 체크포인트 파일 이름
-    save_top_k=1,  # 가장 좋은 k개의 체크포인트만 저장
-    mode='min',  # 모니터링할 메트릭이 낮을수록 좋은 경우 'min'
-    every_n_epochs=10,
-    save_last=True  # 마지막 체크포인트도 저장
-)
+    # ModelCheckpoint callback
+    checkpoint_callback = ModelCheckpoint(
+        monitor='train_loss',
+        dirpath= os.path.join(config['checkpoint_dir'], config['wandb_name']),
+        filename=config['checkpoint_filename'],
+        save_top_k=config['checkpoint_save_top_k'],
+        mode='min',
+        every_n_epochs=config['checkpoint_every_n_epochs'],
+        save_last=True
+    )
 
-# Misc
-base_dir = '/node_data/urp24s_jsjeon/3Difftection_Reproduce/ControlNet2/raw/'
-annotation_file = '/node_data/urp24s_jsjeon/3Difftection_Reproduce/ControlNet2/raw/annotations.json'
-dataset = GeometryDataset(annotation_file, base_dir)
-dataloader = DataLoader(dataset, num_workers=127, batch_size=batch_size, shuffle=True)
-logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(max_epochs = 1000, accelerator="gpu",devices=1, precision=32, callbacks=[logger,checkpoint_callback],logger=wandb_logger)
+    # Misc
+    dataset = GeometryDataset(config['annotation_file'], config['base_dir'])
+    dataloader = DataLoader(dataset, num_workers=config['num_workers'], batch_size=config['batch_size'], shuffle=True)
 
-print(f"Using {trainer.device_ids} GPU(s):")
-    
-# device = trainer.strategy.root_device
-# print(f"Using device: {device}")
+    # Logger
+    logger = ImageLogger(config, batch_frequency=config['logger_freq'])
 
-# Train!
-trainer.fit(model, dataloader)
-wandb.finish()
+    # Trainer
+    trainer = pl.Trainer(
+        max_epochs=config['max_epochs'],
+        accelerator=config['accelerator'],
+        devices=config['devices'],
+        precision=config['precision'],
+        callbacks=[logger, checkpoint_callback],
+        logger=wandb_logger
+    )
+
+    print(f"Using {trainer.device_ids} GPU(s):")
+
+    # Train!
+    trainer.fit(model, dataloader)
+    wandb.finish()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="config file을 입력해주세요.")
+    parser.add_argument("--config", type=str, required=True, help="Path to the config file")
+    args = parser.parse_args()
+
+    main(args.config)

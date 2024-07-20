2195968325a436d4e47be056b01037a3f811808e
Jeon-jisu <wltnjeon0119@gmail.com>
2024-07-20 23:56:36 +0900

    [Feature] config.yaml 파일 반영하도록 변경, combined image 생성 #16 #19

--- ControlNet2/cldm/logger.py
+++ ControlNet2/cldm/logger.py
@@ -7,12 +7,12 @@ from PIL import Image
 from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
-
 class ImageLogger(Callback):
-    def __init__(self, batch_frequency=4000, max_images=4, clamp=True, increase_log_steps=True,
+    def __init__(self, config, batch_frequency=4000, max_images=4, clamp=True, increase_log_steps=True,
                  rescale=True, disabled=False, log_on_batch_idx=False, log_first_step=False,
                  log_images_kwargs=None):
         super().__init__()
+        self.config = config
         self.rescale = rescale
         self.batch_freq = batch_frequency
         self.max_images = max_images
@@ -26,10 +26,17 @@ class ImageLogger(Callback):
 
     @rank_zero_only
     def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
-        root = os.path.join(save_dir, "image_log", split)
+        base_path = os.path.join(save_dir, 'image_log', self.config['wandb_name'], split)
+        
+        # 각 이미지 세트에 대한 고유 폴더 생성
+        unique_folder = f"gs-{global_step:06}_e-{current_epoch:06}_b-{batch_idx:06}"
+        folder_path = os.path.join(base_path, unique_folder)
+        os.makedirs(folder_path, exist_ok=True)
+        
         for k in images:
             if k == "conditioning":
                 continue
+            
             grid = torchvision.utils.make_grid(images[k], nrow=4)
             # 이부분이 정규화 맞춰주는 부분
             if self.rescale:
@@ -37,12 +44,17 @@ class ImageLogger(Callback):
             grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
             grid = grid.numpy()
             grid = (grid * 255).astype(np.uint8)
-            filename = "{}_gs-{:06}_e-{:06}_b-{:06}.png".format(k, global_step, current_epoch, batch_idx)
-            path = os.path.join(root, filename)
-            os.makedirs(os.path.split(path)[0], exist_ok=True)
+            filename = f"{k}.png"
+            path = os.path.join(folder_path, filename)
             Image.fromarray(grid).save(path)
             # WandB에 이미지 로깅
             wandb.log({f"{split}_{k}": wandb.Image(grid)}, step=global_step)
+        # 모든 이미지를 하나의 그리드로 결합
+        combined_grid = self.combine_images(images)
+        combined_path = os.path.join(folder_path, "combined.png")
+        Image.fromarray(combined_grid).save(combined_path)
+        wandb.log({f"{split}/{unique_folder}/combined": wandb.Image(combined_grid)}, step=global_step)
+
             
     @rank_zero_only
     def log_fixed_images(self, pl_module, batch, current_epoch):
